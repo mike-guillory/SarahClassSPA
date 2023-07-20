fe550689313c67e25904cdd0d659109d7ccdad27
SarahSeel <sarahizsilly09@yahoo.com>
2023-07-20 16:35:33 -0500

    update spa

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
+import { Header, Nav, Main, Footer } from "./components";
+import * as store from "./store";
+import Navigo from "navigo";
+import { capitalize } from "lodash";
+
+const router = new Navigo("/");
+
+router
+  .on({
+    "/": () => render(),
+    ":view": params => {
+      let view = capitalize(params.data.view);
+      if (store.hasOwnProperty(view)) {
+        render(store[view]);
+      } else {
+        render(store.Viewnotfound);
+        console.log(`View ${view} not defined`);
+      }
+    }
+  })
+  .resolve();
+
+function render(state = store.Home) {
+  document.querySelector("#root").innerHTML = `
+      ${Header(state)}
+      ${Nav(store.Links)}
+      ${Main(state)}
+      ${Footer()}
+    `;
+
+  router.updatePageLinks();
+}
+
+render();
+
 // add menu toggle to bars icon in nav bar
 document.querySelector(".fa-bars").addEventListener("click", () => {
   document.querySelector("nav > ul").classList.toggle("hidden--mobile");
