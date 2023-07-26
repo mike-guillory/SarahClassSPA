04d36db0316a8054666aa87fb526cf2e414259a1
Matt Thomas <matsinet@gmail.com>
2023-07-26 17:11:55 -0500

    another try

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { Header, Nav, Main, Footer } from "./components";
 import * as store from "./store";
 import Navigo from "navigo";
 import { capitalize } from "lodash";
-import axios from "axios";
+import { default as axios } from "axios";
 
 const router = new Navigo("/");
 
